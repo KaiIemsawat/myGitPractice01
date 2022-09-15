d394e3d608753278b28b683441db68ac072a1224
Kai I <g9mailbox@gmail.com>
2022-09-14 23:33:04 -0400

    still doesnt work

--- src/TestGit01.java
+++ src/TestGit01.java
@@ -1,5 +1,8 @@
 public class TestGit01 {
     public static void main(String[] args) {
         System.out.println("From gitPractice project");
+        System.out.println("Try something new");
+        System.out.println("Try something new02");
+        System.out.println("Try something new03");
     }
 }

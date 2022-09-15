e53192534b34572ac307565d4d07ef3e4faabbd1
Kai I <g9mailbox@gmail.com>
2022-09-14 23:19:16 -0400

    still not working

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("for git practice");
         System.out.println("Update something 01");
+        System.out.println("Update something again");
     }
 }
